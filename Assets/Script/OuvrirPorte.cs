a6c1c49f25b1b3ecc32ede94252903931c2052ea
zaurak0075 <olivier0075@icloud.com>
2023-11-13 11:38:38 -0500

    script

--- Assets/Script/OuvrirPorte.cs
+++ Assets/Script/OuvrirPorte.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class OuvrirPorte : MonoBehaviour
 {
 
-    [SerializeField] GameObject porteDroite;
+    [SerializeField] GameObject PorteDroite;
      [SerializeField] GameObject PorteGauche;
-     [SerializeField] GameObject Joeur
+     [SerializeField] GameObject Joeur;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(porteDroite);
+        Debug.Log(PorteDroite);
         Debug.Log(PorteGauche);
     }
 
      void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag =="Player"){
+        PorteDroite.GetComponent<Animator>().SetBool("Ouvre",true);
+
+        }
     }
 
 }
