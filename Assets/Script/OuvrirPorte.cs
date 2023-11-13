30737761d51dd85e4b80d05242d1a1d0265ca8fc
zaurak0075 <olivier0075@icloud.com>
2023-11-13 10:52:27 -0500

    Script

---
+++ Assets/Script/OuvrirPorte.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OuvrirPorte : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("C'est beau, ça marche! Zbzb");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
